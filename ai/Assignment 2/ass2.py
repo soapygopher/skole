56cae6ce063873cbe3cfd5e25b6cc4f77494c7e4
Håkon Mork <hakon.mork@gmail.com>
2013-02-12 11:14:10 -0500

    now with working value iteration

--- ai/Assignment 2/ass2.py
+++ ai/Assignment 2/ass2.py
@@ -32,22 +32,22 @@ def move(intended):
 		direction = rotate(intended, "right")
 	return direction
 
-def valuefrom(board, x, y, direction):
+def valuefromcell(board, x, y, direction):
 	logging.info("Looking at position " + str(x) + str(y))
 	dy, dx = dirs[direction]
 	ldy, ldx = dirs[rotate(direction, "left")]
 	rdy, rdx = dirs[rotate(direction, "right")]
 	logging.info("Intended direction " + direction + " -> dx = " + str(dx) + ", dy = " + str(dy) + " to " + str(x + dx) + str(y + dy))
-	frontcell, leftcell, rightcell = 0, 0, 0
+	frontcell = leftcell = rightcell = board[y][x]
 	if (0 <= x + dx <= 3) and (0 <= y + dy <= 3):
 		frontcell = board[y + dy][x + dx]
-		frontcell = frontcell if frontcell and (0 <= x + dx <= 3) and (0 <= y + dy <= 3) else 0
+		frontcell = frontcell if frontcell else board[y][x]
 	if (0 <= x + ldx <= 3) and (0 <= y + ldy <= 3):
 		leftcell = board[y + ldy][x + ldx]
-		leftcell = leftcell if leftcell else 0
+		leftcell = leftcell if leftcell else board[y][x]
 	if (0 <= x + rdx <= 3) and (0 <= y + rdy <= 3):
 		rightcell = board[y + rdy][x + rdx]
-		rightcell = rightcell if rightcell else 0
+		rightcell = rightcell if rightcell else board[y][x]
 	logging.info("Right value is " + str(rightcell))
 	logging.info("Front value is " + str(frontcell))
 	logging.info("Left value is " + str(leftcell))
@@ -56,18 +56,23 @@ def valuefrom(board, x, y, direction):
 	return (0.7 * frontcell) + (0.2 * leftcell) + (0.1 * rightcell)
 
 def valueiteration():
-	r = -0.04
+	epsilon = 0.0001
+	delta = 0
+	reward = -0.04
 	gamma = 0.9
 	board = [
-		[r, r,    None, r],
-		[r, r,    r,    r],
-		[r, None, r,   -1],
-		[r, r,    r,    1]
+		[reward, reward, None,   reward],
+		[reward, reward, reward, reward],
+		[reward, None,   reward,     -1],
+		[reward, reward, reward,      1]
 	]
-	print prettyprint(board)
-	print
+	iterationnumber = 1
 	convergence = False
 	while not convergence:
+		delta = 0
+		print "iteration", iterationnumber
+		print prettyprint(board)
+		print
 		nextiterboard = copy.deepcopy(board)
 		for y, line in enumerate(board):
 			for x, cell in enumerate(line):
@@ -77,14 +82,16 @@ def valueiteration():
 				elif (x == 2 and y == 0) or (x == 1 and y == 2):
 					logging.info("Cell " + str(x) + str(y) + " is useless")	
 					continue
-				northval = valuefrom(board, x, y, "N")
-				southval = valuefrom(board, x, y, "S")
-				westval = valuefrom(board, x, y, "W")
-				eastval = valuefrom(board, x, y, "E")
-				cell += gamma * max(eastval, westval, southval, northval)
-				nextiterboard[y][x] = cell
+				northval = valuefromcell(board, x, y, "N")
+				southval = valuefromcell(board, x, y, "S")
+				westval = valuefromcell(board, x, y, "W")
+				eastval = valuefromcell(board, x, y, "E")
+				newvalue = reward + gamma * max(eastval, westval, southval, northval)
+				delta = max(delta, newvalue - nextiterboard[y][x])
+				#print "delta ", delta
+				nextiterboard[y][x] = newvalue
 		board = nextiterboard
-		print prettyprint(board)
-		print
+		convergence = delta < epsilon * (1 - gamma) / gamma
+		iterationnumber += 1
 
 valueiteration()
