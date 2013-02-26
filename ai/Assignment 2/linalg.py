85856488c9f33305ae6a383ddd15882da5ddda71
Håkon Mork <hakon.mork@gmail.com>
2013-02-25 20:07:08 -0500

    extract to problem-specific functions, make iteration function

--- ai/Assignment 2/linalg.py
+++ ai/Assignment 2/linalg.py
@@ -1,4 +1,4 @@
-import numpy
+from numpy import *
 
 a = [[0.8, 0, 0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
 	[0.7, 0.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -17,25 +17,35 @@ a = [[0.8, 0, 0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0.7, 0.2, 0], 
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.7, 0.3]]
 
-r = -0.02
-b = [r,r,r,r,r,0,r,r,r,r,r,0,1,-1,r,r]
+#reward = -0.02
 
-a = numpy.array(a)
-b = numpy.array(b).transpose()
-
-left = numpy.identity(16)
-left *= r
-al = numpy.hstack((left, a))
-
-tempb = b.copy()
-tempb.shape = (4,4)
-print tempb.transpose()[::-1]
-print
-
-x = numpy.dot(a, b)
-while True:
-	x = numpy.dot(a, x)
+def iterate(reward, alt=False):
+	x = [reward,reward,reward,reward,reward,0,reward,reward,reward,reward,reward,0,1,-1,reward,reward]
+	
+	if alt:
+		regulars = array([1 if i == reward else 0 for i in x])
+		regulars.shape = (16, 1)
+	
+	global a
+	locala = copy(a)
+	locala = array(locala) # dimensions (16x16)
+	if alt:
+		locala = hstack((regulars, locala)) # dimensions (16x17)
+	
+	x = array(x) # dimension (16x1)
+	
+	for n in range(1000): # more than enough iterations
+		if alt:
+			x = hstack(([reward], x)).transpose() # x has dimensions (17x1)
+		x = dot(locala, x) # (16x17) x (17x1) = (16x1)
+	
 	tempx = x.ravel()
-	tempx.shape = (4,4)
+	tempx.shape = (4,4) # dimension (4x4) for display
+	print "Reward", reward, "alt =", alt
 	print tempx.transpose()[::-1]
 	print
+
+iterate(-0.02, alt=True)
+iterate(-0.04, alt=True)
+iterate(-0.02, alt=False)
+iterate(-0.04, alt=False)
