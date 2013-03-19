1cfa269419e26012189933a2d35d1366e01dabc3
Håkon Mork <hakon.mork@gmail.com>
2013-03-18 23:54:34 -0400

    images, final version of script and report

--- ai/a3/ass3.m
+++ ai/a3/ass3.m
@@ -1,172 +1,240 @@
-% clear previous context
+% Author: Hakon Mork
+% I think this whole thing requires the Statistics toolbox.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% Setup and initialization %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Flush out any garbage from last run
 clc
 clear
+close all
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%% reading and preparing input %%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% What to do.
+% Use placemeans = true to place computed means (!)
+% on top of the RGB image (for problem 1 and 2),
+placemeans = false;
 
-% read from file into 20x20x3 matrix
+% Use segmenting = true to compute segmentations
+% instead (for problem 3 and 4).
+segmenting = false;
+
+% Use gaussian3d to produce a 3d overlay of the
+% computed weighted-average Gaussian on the input image.
+gaussian3d = true;
+
+
+% Read from image file into 20x20x3 matrix.
+% Change the input file by hand!
 rgbimg = imread('./images/image1_a.png', 'png');
 
-% reshape colors to 400x1 matrices
+% Reshape colors to 400x1 matrices
 reds = reshape(rgbimg(:,:,1)', 400, 1);
 greens = reshape(rgbimg(:,:,2)', 400, 1);
 blues = reshape(rgbimg(:,:,3)', 400, 1);
 
-% x and y indices
+% Vectors of x and y indices
 xs = repmat((1:20)', 20, 1);
 ys = [];
 for i = 1:20
     ys = [ys; ones(20, 1) * i];
 end
 
-% concatenate to 400x5 matrix of [r, g, b, x, y] values
+% Concatenate to 400x5 matrix of [r, g, b, x, y] values
 rgbxy = [reds, greens, blues, xs, ys];
 rgbxy = double(rgbxy);
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%% setting up EM %%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% number of clusters/gaussians
+% Number of clusters to divide the image into
 clusters = 4;
 
-% 5 values for each data point: r, g, b, x, y
-datapoints = 5;
 
-% 5-by-#clusters matrix of means: rgbxy mean values for each cluster
-%mus = rand(datapoints, clusters);
-% multiply by 20 so we spread them out across the image
-mus = rand(datapoints, clusters) * 20;
-%mus(:,1)
-mus(1:3,:) = 255;
 
-% covariance matrices, one for each gaussian
-% sigmas = [];
-% for i = 1:clusters
-%     % we need a 2x2 symmetric positive definite matrix
-%     % any matrix multiplied by its transpose has this property
-%     m = randn(datapoints);
-%     newsigma = m' * m;
-%     % we multiply it by some number,
-%     % so it doesn't get so sharp and thin
-%     sigmas(:,:,i) = newsigma * 10;
-% end
-for k = 1:clusters
-    sigmas(:,:,k) = eye(datapoints);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% EM algorithm %%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Construct initial guesses for means, covariances and
+% weights for determining the impact of the initial parameters.
+
+% These values are what the 'randSample' parameter give
+% to gmdistribution.fit, but we need to construct
+% them by hand to plot them with the convergence points.
+
+% Pick random data points as initial means
+randindex = randperm(length(rgbxy));
+startmu = rgbxy(randindex(1:clusters),:);
+
+% Initialize covariance matrices to variances of each component
+startsigmas = eye(5);
+for i = 1:clusters
+    I = eye(5);
+    I(I == 1) = var(rgbxy);
+    startsigmas(:,:,i) = I;
 end
 
-% weight parameters
-weights = ones(clusters, 1);
+% Assume equal weight for each gaussian
+startweights = ones(clusters, 1) / clusters;
+
+% EM argument structure
+startstruct = struct('mu', startmu, ...
+    'Sigma', startsigmas, ...
+    'PComponents', startweights);
+
+% Compute cluster gaussians, with or without
+% a random initial guess of parameters, i.e. fit
+% the data in rgbxy to a number of different clusters,
+% starting with the initial guesses in startstruct.
+obj = gmdistribution.fit(rgbxy, clusters, ...
+    'Start', startstruct, ...
+    'Regularize', 1e-7);
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%% EM algorithm itself %%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-iterations = 0;
-converged = false;
-while ~converged
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% Visualization, problem 1 %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if placemeans
+    % Initial and converged means for plotting
+    meanx_init = startmu(:,4); % initial guess x-means
+    meany_init = startmu(:,5); % initial guess y-means
+    meanx_conv = obj.mu(:,4); % converged x-means
+    meany_conv = obj.mu(:,5); % converged y-means
     
-    for i = 1:clusters
-        
-        %%%%%%% setup %%%%%%%
-        
-        mu = mus(:,i);
-        sigma = sigmas(:,:,i);
-        
-        % normalization constant
-        alpha = 1;
-        
-        
-        %%%%%%% e step %%%%%%%
-        
-        % probabilities of this distribution at each sample
-        p_ijs = zeros(length(rgbxy), 1);
-        
-        % make all the p_ij values
-        sigma
-        inv(sigma)
-        for j = 1:length(rgbxy)
-            % get the datapoint x_j on the right format
-            x_j = rgbxy(i,:)';
-            % probability at x of the i-th gaussian
-            % i.e., P(x_j | C = i)
-            % using multivariate normal distribution
-            p_ijs(i) = mvnpdf(x_j, mu, sigma);
-            %p_ijs(i)
+    
+    % Display the rgb image itself as a backdrop
+    imshow(rgbimg, ...
+        'InitialMagnification', 2000, ...
+        'Border', 'tight');
+    
+    % Put the convergeed mean x and y values on top of
+    % the image as green dots, and the initial guesses
+    % as green squares
+    hold on
+    scatter(meanx_init, meany_init, 100, 'gs', 'fill')
+    scatter(meanx_conv, meany_conv, 100, 'go', 'fill')
+    hold off
+    
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% Visualization, problem 3 %%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if segmenting
+    
+    % Argmin mean of norm between data and means
+    closest = 1:400;
+    segmented = zeros(400, 3);
+    for i = 1:400
+        mindist = 1e300;
+        for j = 1:clusters
+            dist = norm(rgbxy(i,:) - obj.mu(j,:));
+            if dist < mindist
+                mindist = dist;
+                closest(i) = j;
+            end
         end
-        
-        % adjust with normalization and weight parameters
-        % p_ij = alpha * P(x_j | C = i) * P(C = i)
-        %      = alpha * P(x_j | C = i) * w_i
-        %p_ijs = alpha * p_ijs * weights(i);
-%         p_ijs
-%         alpha
-%         weights(i)
-        
-        % sum of all probabilities for this gaussian
-        p_i = sum(p_ijs);
-        i
-        p_i
-        
-        
-        %%%%%%% m step %%%%%%%
-        
-        newmu = 0;
-        newsigma = zeros(datapoints); % 5x5
-        for j = 1:length(p_ijs)
-            x_j = rgbxy(j,:);
-            p_ij = p_ijs(j);
-            newmu = newmu + (p_ij * x_j) / p_i;
-            %newmu = newmu + (p_ij * x_j);
-            newsigma = newsigma + (p_ij * (x_j - mu')' * (x_j - mu')) / p_i;
-            %newsigma = newsigma + (p_ij * (x_j - mu')' * (x_j - mu'));
-        end        
-        %newsigma = newsigma / p_i;
-        %newmu = newmu / p_i;
-        
-        % update for next iteration
-        mus(:,i) = newmu;
-        sigmas(:,:,i) = newsigma;
-        weights(i) = p_i;
-        
+        segmented(i,:) = obj.mu(closest(i),1:3);
     end
     
-    iterations = iterations + 1;
-    %converged = true;
-    converged = (iterations == 5);
+    % Rounding
+    segmented = uint8(segmented);
+    
+    rs = reshape(segmented(:,1), 20, 20)';
+    gs = reshape(segmented(:,2), 20, 20)';
+    bs = reshape(segmented(:,3), 20, 20)';
+    
+    % Concatenate in third dimension
+    segrgb = cat(3, rs, gs, bs);
+    
+    % Save figure manually from popup window
+    imshow(segrgb, ...
+        'InitialMagnification', 2000, ...
+        'Border', 'tight');
 end
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%% result visualization %%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% Visualization for fancypants %%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% % bounds and grid
-% lower = 1;
-% upper = 20;
-% res = 0.2;
-% [x, y] = meshgrid(lower:res:upper, lower:res:upper);
-%
-% % add up all the distributions
-% f = 0;
-% for i = 1:clusters
-%     mu = mus(:, i);
-%     sigma = sigmas(:,:,i);
-%     % multivariate normal distribution
-%     % with the given mean and covariance matrices
-%     g = mvnpdf([x(:), y(:)], mu, sigma);
-%     % reshape to square
-%     g = reshape(g, length(y), length(x));
-%     % and add to the final value
-%     f = f + g;
-% end
-%
-% %contour(x, y, f)
-% surf(x, y, f)
-
-sprintf('done')
+if gaussian3d
+    
+    % Draw 1000 samples according to the Gaussian distribution
+    % we computed earlier. We'll use these data points to
+    % make a new distribution object to plot.
+    % It's a bit convoluted, but nevermind.
+    samples = [];
+    for k = 1:clusters
+        sample_mu = [obj.mu(k,4), obj.mu(k,5)];
+        sample_sigma = obj.Sigma(:,:,k);
+        sample_sigma = sample_sigma(4:5, 4:5);
+        samples = [samples; mvnrnd(sample_mu, sample_sigma, 1000)];
+    end
+    
+    samples_xs = samples(:,1);
+    samples_ys = samples(:,2);
+    samples_obj = gmdistribution.fit(samples, clusters);
+    
+    range = [0, 20];
+    funchandle = @(x, y) pdf(samples_obj, [x y]);
+    
+    % Plot 3d mesh grid with accompanying contour lines
+    gaussianhandle = ezmeshc(funchandle, range, range);
+    
+    % Slight transparency because I'm being fancy
+    set(gaussianhandle, 'FaceAlpha', 0.6);
+    
+    % We're not done placing stuff on the figure
+    hold on
+    
+    % Z-level to place the contour on
+    contourlevel = -0.005;
+    
+    % Set all the contour lines on the bottom of the
+    % figure, instead of on z = 0.
+    contourhandle = findobj('type', 'patch');
+    zd = get(contourhandle, 'ZData');
+    for i = 1:length(zd)
+        set(contourhandle(i), 'ZData', ...
+            contourlevel * ones(length(zd{i}), 1))
+    end
+    
+    % We apparently need the transpose of the input image,
+    % for some reason I'm not too clear on.
+    disprgb(:,:,1) = rgbimg(:,:,1)';
+    disprgb(:,:,2) = rgbimg(:,:,2)';
+    disprgb(:,:,3) = rgbimg(:,:,3)';
+    
+    % Now, let's place the original image on the bottom of the
+    % plot so we see how the Gaussian corresponds nicely
+    % (hopefully) to the color blocks. We may have to run
+    % it a couple of times to get nice results.
+    
+    % Got the idea for this part from StackOverflow,
+    % http://stackoverflow.com/questions/3719502/how-can-i-plot-an-image-
+    % jpg-in-matlab-in-both-2-d-and-3-d
+    
+    % The x data for the image corners
+    xImage = [0 20; 0 20];
+    % The y data for the image corners
+    yImage = [0 0; 20 20];
+    % The z data for the image corners
+    zImage = [contourlevel, contourlevel; ...
+        contourlevel, contourlevel];
+    
+    surf(xImage, yImage, zImage,...
+        'CData', disprgb, ...
+        'FaceColor', 'texturemap')
+    
+    title '' % Don't need a title
+    
+    axis([0, 20, 0, 20, contourlevel, 0.01])
+    view([118, 20]) % Tilt so it's nicer to look at
+    
+end
 
